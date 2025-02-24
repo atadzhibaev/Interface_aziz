8649138af3fc1ccf8723f6f56d96b2c10a7085e9
jfessler1 <fesslerjo2024@gmail.com>
2025-02-24 11:35:44 -0600

    Fixed cat sound and made it easier to say yes or no

--- src/AnimalDriver.java
+++ src/AnimalDriver.java
@@ -4,27 +4,27 @@ public class AnimalDriver {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String asnwer = new String();
-        System.out.print("Want to hear a dog barking (yes/no) ? ");
+        System.out.print("Want to hear a dog barking (y/n) ? ");
         asnwer = scan.nextLine();
-        if (asnwer.equals("yes")) {
+        if (asnwer.equals("y")) {
             Animal charlie = new Dog();
             charlie.printImage();
             charlie.makeSound();
             charlie.makeMove();
         }
 
-        System.out.print("Want to hear a cat meowing (yes/no) ? ");
+        System.out.print("Want to hear a cat meowing (y/n) ? ");
         asnwer = scan.nextLine();
-        if (asnwer.equals("yes")) {
+        if (asnwer.equals("y")) {
             Animal kuzzi = new Cat();
             kuzzi.printImage();
             kuzzi.makeSound();
             kuzzi.makeMove();
         }
 
-        System.out.print("Want to hear a chicken cluck (yes/no) ? ");
+        System.out.print("Want to hear a chicken cluck (y/n) ? ");
         asnwer = scan.nextLine();
-        if (asnwer.equals("yes")) {
+        if (asnwer.equals("y")) {
             Animal chicken = new Chicken();
             chicken.printImage();
             chicken.makeSound();
